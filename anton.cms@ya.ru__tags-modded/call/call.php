25348092de6db28bf4c6823d110b5ddb756e8d3d
trendoman <tony.smirnov@gmail.com>
2022-06-28 18:15:24 +0300

    Update 'Modded/call' tag

--- anton.cms@ya.ru__tags-modded/call/call.php
+++ anton.cms@ya.ru__tags-modded/call/call.php
@@ -10,18 +10,15 @@
    $FUNCS->add_event_listener( 'alter_tag_call_execute', function($tag_name, &$params, $node, &$html){
       global $FUNCS;
 
-      if (!is_null($params[0]['lhs']) ) return $break = false; // only user-defined functions
       $name = trim( $params[0]['rhs'] );
-      if( !$name ) return $break = true;
-      if( !array_key_exists($name, $FUNCS->funcs) ) return $break = true;
+      if( !$name || !array_key_exists($name, $FUNCS->funcs) ) return $stop_propagation = false;
 
       $func = $FUNCS->funcs[$name];
       $_param = array_shift( $params );
-
       $params = _get_named_params( $func['params'], $params );
       array_unshift($params, $_param);
 
-      return $break = false;
+      return $stop_propagation = false;
    });
 
    $FUNCS->add_event_listener( 'tag_call_executed', function($tag_name, &$params, $node, &$html){
